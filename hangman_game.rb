e4180f0cf6f31fabfd4e1ab73c5639ea58826a89
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-05-19 11:19:21 -0700

    Add ASCII art

--- hangman_game.rb
+++ hangman_game.rb
@@ -1,19 +1,21 @@
 class HangmanGame
   INCORRECT_GUESS_LIMIT = 6
   
-  attr_reader :guesses, :win, :lose
+  attr_reader :incorrect_letters, :win, :lose
   
   def initialize
     @win = false
     @lose = false
     @word = random_word
-    @incorrect_guesses = 0
+    @incorrect_letters = []
     @guessed_letters = []
     @progress = Array.new(@word.length)
   end
   
   def to_s
     result = ""
+
+    result += hangman_ascii(@incorrect_letters.length)
     
     progress_string = @progress.map do |letter|
       if letter
@@ -24,20 +26,22 @@ class HangmanGame
     end
     result += progress_string.join(" ") + "\n"
     
-    guesses_left = INCORRECT_GUESS_LIMIT - @incorrect_guesses
-    result + "Incorrect guesses left: #{guesses_left}"
+    guesses_left = INCORRECT_GUESS_LIMIT - @incorrect_letters.length
+    result += "Incorrect letters: #{incorrect_letters.join(", ")}\n"
+    result += "Incorrect guesses left: #{guesses_left}"
   end
   
   def guess(guess_letter)
     guess_letter.upcase!
     return false if @guessed_letters.include?(guess_letter)
+    @guessed_letters << guess_letter
     
     if @word.include?(guess_letter)
       @word.split("").each_with_index do |letter, i| 
         @progress[i] = guess_letter if letter == guess_letter
       end
     else
-      @incorrect_guesses += 1
+      @incorrect_letters << guess_letter
     end
     
     check_game_end
@@ -58,9 +62,17 @@ class HangmanGame
   def check_game_end
     if @progress.all?
       win = true
-    elsif @incorrect_guesses >= INCORRECT_GUESS_LIMIT
+    elsif @incorrect_letters.length >= INCORRECT_GUESS_LIMIT
       lose = true
     end
+  end
+
+  def hangman_ascii(num)
+    result = ""
+    f = File.open("ascii/ascii_hangman#{num}.txt", "r")
+    result << f.read
+    f.close
+    result
   end
     
     
